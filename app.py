320ba7ab35226e95b4d887ba6d90f24ef35266cc
RDas <rudrangshu.das@gmail.com>
2025-08-20 00:15:45 +0530

    restoring model.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #from cgitb import text
 from flask import Flask,render_template,request
-import model_nltk as model
+import model
 import model_spacy
 app = Flask('Product_Recommendation')
 
@@ -9,23 +9,6 @@ valid_userid = ['00sab00','1234','zippy','zburt5','joshua','dorothy w','rebecca'
 def view():
     return render_template('index.html')
 
-# @app.route('/recommend',methods=['POST'])
-# def recommend_top5():
-#     print(request.method)
-#     user_name = request.form['User Name']
-#     print('User name=',user_name)
-    
-#     if  user_name in valid_userid and request.method == 'POST':
-#             top20_products = model.recommend_products(user_name)
-#             print(top20_products.head())
-#             get_top5 = model.top5_products(top20_products)
-#             #return render_template('index.html',tables=[get_top5.to_html(classes='data',header=False,index=False)],text='Recommended products')
-#             return render_template('index.html',column_names=get_top5.columns.values, row_data=list(get_top5.values.tolist()), zip=zip,text='Recommended products')
-#     elif not user_name in  valid_userid:
-#         return render_template('index.html',text='No Recommendation found for the user')
-#     else:
-#         return render_template('index.html')
-    
 @app.route('/recommend',methods=['POST'])
 def recommend_top5():
     print(request.method)
@@ -33,15 +16,16 @@ def recommend_top5():
     print('User name=',user_name)
     
     if  user_name in valid_userid and request.method == 'POST':
-            top20_products = model_spacy.recommend_products(user_name)
+            top20_products = model.recommend_products(user_name)
             print(top20_products.head())
-            get_top5 = model_spacy.top5_products(top20_products)
+            get_top5 = model.top5_products(top20_products)
             #return render_template('index.html',tables=[get_top5.to_html(classes='data',header=False,index=False)],text='Recommended products')
             return render_template('index.html',column_names=get_top5.columns.values, row_data=list(get_top5.values.tolist()), zip=zip,text='Recommended products')
     elif not user_name in  valid_userid:
         return render_template('index.html',text='No Recommendation found for the user')
     else:
-        return render_template('index.html')
+        return render_template('index.html') 
+
 
 
 if __name__ == '__main__':
